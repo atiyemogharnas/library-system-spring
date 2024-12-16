3b22f7adb68513106e90bd14f22c43d5b8250d21
atiye mogharnas <atiyemogharnas@gmail.com>
2024-12-16 17:51:28 +0330

    add gc.log

--- src/main/java/com/library/system/controller/BookController.java
+++ src/main/java/com/library/system/controller/BookController.java
@@ -8,7 +8,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/book")
+@RequestMapping("/librarySystem/book")
 @Slf4j
 public class BookController {
 
--- src/main/java/com/library/system/controller/ItemController.java
+++ src/main/java/com/library/system/controller/ItemController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/libraryItem")
+@RequestMapping("/librarySystem/item")
 @Slf4j
 public class ItemController {
 
